e4d6ef46b94106af293c0e9515ad31cab113c1ef
Etienne Mueller <etienne.mueller@outlook.com>
2024-10-14 13:40:51 +1100

    fixed script to work with UoM

--- scratch/process_stitched_vol.py
+++ scratch/process_stitched_vol.py
@@ -1,40 +1,45 @@
-import rawpy
-import imageio
+"""Script for turning a raw file into a zarr dataset"""
+#import imageio
 import numpy as np
-from tqdm import tqdm
+#import rawpy
 import zarr
 
 from pathlib import Path
+from tqdm import tqdm
 
-# Script for turning a raw file into a zarr dataset
 
-path = Path(
-    "/nrs/funke/pattonw/data/zebrafish/stitched/top_left_right_bottom_resliced_8555x5155x4419.raw"
-)
-container = zarr.open(
-    "/nrs/funke/pattonw/predictions/zebrafish/zebrafish.n5", mode="r+"
-)
-output_data = container.create_dataset(
-    "volumes/s17/raw", dtype=np.uint8, overwrite=True, shape=(8555, 5155, 4419)
-)
-
-n_z = 8555
 size_y = 5155
 size_x = 4419
 size_z = 8555
-count = size_x * size_y
-start_z = 0
-end_z = size_z
-n_bytes = 1  # Number of bytes in a uint8 (for offset)
-fd = open(path, "rb")
-fd.seek(start_z)
 
-for i in tqdm(range(start_z, end_z, n_z), desc=path.stem):
-    offset = 0
-    data = (
-        np.fromfile(fd, dtype="b", offset=offset, count=n_z * count)
-        .reshape(size_x, size_y, -1)
-        .transpose((2, 1, 0))
-    )
-    output_data[i : i + data.shape[0]] = data
-fd.close()
+base_path = "/data/projects/punim2142/zebrafish_experiments/data/"
+path = Path(base_path + "top_left_right_bottom_resliced_8555x5155x4419.raw")
+container = zarr.open(base_path + "zebrafish_out.n5", mode="a")
+
+output_data = container.create_dataset("volumes/s17/raw", 
+                                       dtype=np.uint8,
+                                       overwrite=True,
+                                       shape=(size_z, size_y, size_x))
+
+
+def main():
+    #n_z = 8555
+    count = size_x * size_y
+    start_z = 0
+    #end_z = size_z
+    #n_bytes = 1  # Number of bytes in a uint8 (for offset)
+
+    fd = open(path, "rb")
+    fd.seek(start_z)
+
+    #for i in tqdm(range(start_z, end_z, n_z), desc=path.stem):
+    for i in tqdm(range(start_z, size_z, size_z), desc=path.stem):
+        offset = 0
+        #data = (np.fromfile(fd, dtype="b", offset=offset, count=n_z * count).reshape(size_x, size_y, -1).transpose((2, 1, 0)))
+        data = (np.fromfile(fd, dtype="b", offset=offset, count=size_z * count).reshape(size_x, size_y, -1).transpose((2, 1, 0)))
+        output_data[i : i + data.shape[0]] = data
+    fd.close()
+
+
+if __name__ == "__main__":
+    main()
